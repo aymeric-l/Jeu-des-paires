d95145eb56ec6c5132b2bc18b78e6ca1608032b9
Lacoste Aymeric <aymeric-lacoste@hotmail.fr>
2017-01-28 19:17:27 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,21 +16,29 @@ var varOurs = 100;
 var varOursDeux = 100;
 var compteur = 1;
 
-$('#blackAne').click(function(){
-	$('#blackAne').css('z-index', 1);
-	if(varGlobale == 100){
-		varGlobale = 333;
-		varAne = 333;
+function win (){		
+if(compteur == 8){									// Si toutes les cartes sont retournées	
+	console.log('GG WP');
+	$('body').css('background-color','darkblue');	// Annonce la victoire au joueur par un magnifique fond bleu
+	$('#contenaire').css('display','none');			
 	}
-	else if(varGlobale == 333 && varAneDeux != 333){
-		setTimeout(function(){
-			$('.black').css('z-index', 3);
+}
+
+$('#blackAne').click(function(){		//	Si on click sur la div #blackAne, alors : 
+	$('#blackAne').css('z-index', 1);	//	Passe le z-index à 1 -> fait donc apparaitre la photo
+	if(varGlobale == 100){				//	Si aucune carte n'est retourné
+		varGlobale = 333;				//  On sauvegarde le fait d'avoir retourné cette premiere carte
+		varAne = 333;					//	On dit que la carte retournée est "Ane"
+	}
+	else if(varGlobale == 333 && varAneDeux != 333){ 	// Si une carte était déjà retournée MAIS que ce n'était pas sa soeur 
+		setTimeout(function(){							
+			$('.black').css('z-index', 3);		// Attend une seconde et cache toutes les cartes
 		},1000); 
-		varGlobale = 100;
-		varChien = 100;
-		varChienDeux = 100;
-		varAne = 100;
-		varAneDeux = 100;
+		varGlobale = 100;								
+		varChien = 100;			// Sauvegarde le fait que toutes les cartes soit maintenant cachées
+		varChienDeux = 100;		//	
+		varAne = 100;			//
+		varAneDeux = 100;		//
 		varChat = 100;
 		varChatDeux = 100;
 		varLama = 100;
@@ -42,13 +50,13 @@ $('#blackAne').click(function(){
 		varOurs = 100;
 		varOursDeux = 100;
 	}
-	else if(varGlobale == 333 && varAneDeux == 333){
-		$('#blackAne').css('display', 'none');
-		$('#blackAne2').css('display', 'none');
-		varGlobale = 100;
-		compteur++;
+	else if(varGlobale == 333 && varAneDeux == 333){	// Si une carte est déjà retournée et que c'est bien sa soeur
+		$('#blackAne').css('display', 'none');			
+		$('#blackAne2').css('display', 'none');			//	Affiche définitivement ces deux cartes, en mettant display none aux caches-cartes
+		varGlobale = 100;	// Sauvegarde qu'aucune carte n'est retournée
+		compteur++;			// Ajoute un point au compteur
 		console.log(compteur);
-		win();
+		win();				// Annonce la victoire si toutes les cartes du jeu sont retournées
 	}
 });
 
@@ -548,10 +556,4 @@ $('#blackOurs2').click(function(){
 	}
 });
 
-function win (){
-if(compteur == 8){
-	console.log('GG WP');
-	$('body').css('background-color','darkblue');
-	$('#contenaire').css('display','none');
-	}
-}
+
