b68bee062f54a3bcbad9adca5a67a333febe88d9
Lacoste Aymeric <aymeric-lacoste@hotmail.fr>
2017-01-29 21:21:02 +0100

    Correction bug

--- js/script.js
+++ js/script.js
@@ -1,37 +1,29 @@
 /*var varChien, varChienDeux, varGlobale, varAne, varAneDeux = 100;*/
-var varGlobale = 100;
-var varChien = 100;
-var varChienDeux = 100;
-var varAne = 100;
-var varAneDeux = 100;
-var varChat = 100;
-var varChatDeux = 100;
-var varLama = 100;
-var varLamaDeux = 100;
-var varLapins = 100;
-var varLapinsDeux = 100;
-var varLionne = 100;
-var varLionneDeux = 100;
-var varOurs = 100;
-var varOursDeux = 100;
+var varGlobale = varChien = varChienDeux = varAne = varAneDeux = varChat = varChatDeux = varLama = varLamaDeux = 
+varLapins = varLapinsDeux = varLionne = varLionneDeux = varOurs = varOursDeux = 100;
 var compteur = 1;
 var largeur = -54;
 var hauteur = 100;
 var compteurPourHauteur = 0;
+var dispo = true;
 
 
 ////////////        Mélange du tableau		////////////////////
+
 var cards = ["chat", "chat2", "lionne", "lionne2", "chien", "chien2", "lapins", "lapins2", "lama", "lama2", "ane", "ane2", "ours", "ours2"];
 cards = cards.sort(function() { return 0.5 - Math.random() });
 
 ///////////   		Ecriture du HTML		////////////////////
+
 for(i=0;i<cards.length;i++){
 	largeur = largeur + 154;  // Calcul du placement des images
 	compteurPourHauteur++;
 		if(compteurPourHauteur == 8){hauteur = 254;largeur = 100} // Si on a une ligne de 7 images, on fait une nouvelle ligne	
 	$("#contenaire").append('<img src="img/'+cards[i]+'.jpg" id="'+cards[i]+'" style="left: '+largeur+
-	'px; top: '+hauteur+'px;"><div id="black'+cards[i]+'" class="black" style="left:'+largeur+'px; top: '+hauteur+'px"></div>');
+	'px; top: '+hauteur+'px;"><div id="black'+cards[i]+'" class="black" style="left:'+largeur+'px; top: '+hauteur+'px"></div>'); // Ecrit le html
 }
+
+//////////////////////////////////////
 
 function win (){		
 if(compteur == 8){									// Si toutes les cartes sont retournées	
@@ -41,20 +33,26 @@ if(compteur == 8){									// Si toutes les cartes sont retournées
 	}
 }
 
+////////////////////////////////////////////////////////
+
 function retry(){
 	location.reload();			// Recharge la page html
 }
 
 //////	Une fonction pour chaque carte ZzzzzZZzzzZZzzzzzz
+/////////////////////////////////////////////////////////
 
-$('#blackane').click(function(){		//	Si on click sur la div #blackAne, alors : 
+$('#blackane').click(function(){
+if(dispo){							//	Si on click sur la div #blackAne, alors : 
 	$('#blackane').css('z-index', 1);	//	Passe le z-index à 1 -> fait donc apparaitre la photo
 	if(varGlobale == 100){				//	Si aucune carte n'est retourné
 		varGlobale = 333;				//  On sauvegarde le fait d'avoir retourné cette premiere carte
 		varAne = 333;					//	On dit que la carte retournée est "Ane"
 	}
 	else if(varGlobale == 333 && varAneDeux != 333){ // Si une carte était déjà retournée MAIS que ce n'était pas sa soeur 
-		setTimeout(function(){							
+		dispo = false;
+		setTimeout(function(){
+		dispo = true;							
 			$('.black').css('z-index', 3);		// Attend une seconde et cache toutes les cartes
 		},1000); 
 		varGlobale = 100;								
@@ -80,21 +78,24 @@ $('#blackane').click(function(){		//	Si on click sur la div #blackAne, alors :
 		compteur++;			// Ajoute un point au compteur
 		console.log(compteur);
 		win();				// Annonce la victoire si toutes les cartes du jeu sont retournées
-	}
+	}}
 });
 
 //////////////////////////////	 ET CA CONTINUE, ENCORE ET ENCORE. C'EST QUE LE DEBUT, D'ACCORD D'ACCORD.  ////////////////////////////////
 
 $('#blackane2').click(function(){
+	if(dispo){
 	$('#blackane2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varAneDeux = 333;
 	}
 	else if(varGlobale == 333 && varAne != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
-		},1000); 
+		},1300); 
 		varGlobale = 100;
 		varChien = 100;
 		varChienDeux = 100;
@@ -118,19 +119,22 @@ $('#blackane2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////	ET CA CONTINUE, ENCORE ET ENCORE. C'EST QUE LE DEBUT, D'ACCORD D'ACCORD.  ///////////////////
 
 $('#blackchat').click(function(){
+	if(dispo){
 	$('#blackchat').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varChat = 333;
 	}
 	else if(varGlobale == 333 && varChatDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -156,19 +160,22 @@ $('#blackchat').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blackchat2').click(function(){
+	if(dispo){
 	$('#blackchat2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varChatDeux = 333;
 	}
 	else if(varGlobale == 333 && varChat != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -194,19 +201,22 @@ $('#blackchat2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blackchien').click(function(){
+	if(dispo){
 	$('#blackchien').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varChien = 333;
 	}
 	else if(varGlobale == 333 && varChienDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('#blackChien2').css('z-index', 3);
 			$('.black').css('z-index', 3);
 		},1000); 
@@ -232,20 +242,23 @@ $('#blackchien').click(function(){
 		varGlobale = 100;
 		compteur++;
 		console.log(compteur);
-		win();
+		win();}
 	}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blackchien2').click(function(){
+	if(dispo){
 	$('#blackchien2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varChienDeux = 333;
 	}
 	else if(varGlobale == 333 && varChien != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('#blackChien2').css('z-index', 3);
 			$('.black').css('z-index', 3);
 		},1000); 
@@ -271,20 +284,23 @@ $('#blackchien2').click(function(){
 		varGlobale = 100;
 		compteur++;
 		console.log(compteur);
-		win();
+		win();}
 	}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklama').click(function(){
+	if(dispo){
 	$('#blacklama').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLama = 333;
 	}
 	else if(varGlobale == 333 && varLamaDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -310,19 +326,22 @@ $('#blacklama').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklama2').click(function(){
+	if(dispo){
 	$('#blacklama2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLamaDeux = 333;
 	}
 	else if(varGlobale == 333 && varLama != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -348,19 +367,22 @@ $('#blacklama2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklapins').click(function(){
+	if(dispo){
 	$('#blacklapins').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLapins = 333;
 	}
 	else if(varGlobale == 333 && varLapinsDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -386,19 +408,22 @@ $('#blacklapins').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklapins2').click(function(){
+	if(dispo){
 	$('#blacklapins2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLapinsDeux = 333;
 	}
 	else if(varGlobale == 333 && varLapins != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -424,19 +449,22 @@ $('#blacklapins2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklionne').click(function(){
+	if(dispo){
 	$('#blacklionne').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLionne = 333;
 	}
 	else if(varGlobale == 333 && varLionneDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -462,19 +490,22 @@ $('#blacklionne').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blacklionne2').click(function(){
+	if(dispo){
 	$('#blacklionne2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varLionneDeux = 333;
 	}
 	else if(varGlobale == 333 && varLionne != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -500,19 +531,22 @@ $('#blacklionne2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blackours').click(function(){
+	if(dispo){
 	$('#blackours').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varOurs = 333;
 	}
 	else if(varGlobale == 333 && varOursDeux != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -538,19 +572,22 @@ $('#blackours').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 //////////////////////////////////////////////////////////////////////
 
 $('#blackours2').click(function(){
+	if(dispo){
 	$('#blackours2').css('z-index', 1);
 	if(varGlobale == 100){
 		varGlobale = 333;
 		varOursDeux = 333;
 	}
 	else if(varGlobale == 333 && varOurs != 333){
+		dispo = false;
 		setTimeout(function(){
+			dispo = true;
 			$('.black').css('z-index', 3);
 		},1000); 
 		varGlobale = 100;
@@ -576,7 +613,7 @@ $('#blackours2').click(function(){
 		compteur++;
 		console.log(compteur);
 		win();
-	}
+	}}
 });
 
 
